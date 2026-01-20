94745c289a0402e9634609e5090b15fb042427e3
Kimsubinnn <bella8010@naver.com>
2026-01-20 18:32:00 +0900

    refactor(chat): chat Service 수정

--- src/main/java/com/sanaiclub/chat/service/ChatService.java
+++ src/main/java/com/sanaiclub/chat/service/ChatService.java
@@ -118,6 +118,6 @@ public class ChatService {
     // =========================================
     private Integer getLogin_user_id() {
         // 실제 구현에서는 SecurityContext, Session 등에서 가져오기
-        return 2; // 테스트용 하드코딩
+        return 11; // 테스트용 하드코딩
     }
 }
