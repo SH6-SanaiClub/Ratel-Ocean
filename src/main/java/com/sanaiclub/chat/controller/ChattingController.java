cf1abb062646294d05aec7227448f83f873e1a65
Kimsubinnn <bella8010@naver.com>
2026-01-20 09:37:27 +0900

    feat(chat):chat controller 생성

---
+++ src/main/java/com/sanaiclub/chat/controller/ChattingController.java
@@ -0,0 +1,91 @@
+package com.sanaiclub.chat.controller;
+
+import com.sanaiclub.chat.model.dto.ChatMessageDTO;
+import com.sanaiclub.chat.model.dto.ChatRoomDTO;
+import com.sanaiclub.chat.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+@Controller
+@RequestMapping("/chat")
+@RequiredArgsConstructor
+public class ChattingController {
+
+    private final ChatService chatService;
+
+    // 채팅 아이콘 → 목록 화면
+    @GetMapping
+    public String roomList() {
+        return "chat/roomList";
+    }
+
+    // 채팅방 진입 (화면)
+    @GetMapping("/room/{room_id}")
+    public String room(@PathVariable Integer room_id, Model model) {
+
+        // 1. 채팅방 기본 정보 (상대방, 프로젝트 포함)
+        ChatRoomDTO room = chatService.find_room_by_id(room_id);
+
+        // 2. 공유 파일 목록
+        List<ChatMessageDTO> sharedFiles = chatService.find_shared_files(room_id);
+
+        model.addAttribute("room_id", room_id);
+        model.addAttribute("room", room);
+        model.addAttribute("sharedFiles", sharedFiles);
+
+        return "chat/room";
+    }
+
+    // 채팅방 목록 데이터 (AJAX)
+    @GetMapping("/rooms")
+    @ResponseBody
+    public List<ChatRoomDTO> rooms() {
+        return chatService.find_my_rooms();
+    }
+
+    // 메시지 목록 조회
+    @GetMapping("/room/{room_id}/messages")
+    @ResponseBody
+    public List<ChatMessageDTO> getMessages(@PathVariable Integer room_id) {
+        return chatService.find_messages(room_id);
+    }
+
+    // 메시지 전송
+    @PostMapping("/room/{room_id}/message")
+    @ResponseBody
+    public void sendMessage(
+            @PathVariable Integer room_id,
+            @RequestParam(required = false) String content,
+            @RequestParam(required = false) MultipartFile file
+    ) throws IOException{
+        String file_name = null;
+        String file_url = null;
+        Long file_size = null;
+
+        // 파일 처리
+        if (file != null && !file.isEmpty()) {
+            file_name = file.getOriginalFilename();
+            file_size = file.getSize();
+
+            String uploadDir = "C:/upload/chat";
+            File dir = new File(uploadDir);
+            if (!dir.exists()) dir.mkdirs();
+            File savedFile = new File(uploadDir, file_name);
+            file.transferTo(savedFile);
+            file_url = "/upload/chat/" + file_name;
+        }
+            chatService.send_message(room_id, content, file_name, file_url, file_size);
+
+
+    }
+
+
+}
+
