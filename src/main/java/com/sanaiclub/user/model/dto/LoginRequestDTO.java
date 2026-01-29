f9c089fee27710e3a2a7ad2ba4e603f4461f9698
sttarrynight <suwo5612@naver.com>
2026-01-29 16:30:37 +0900

    refactor(user) : 로그인 시 글자 수 제한 삭제

--- src/main/java/com/sanaiclub/user/model/dto/LoginRequestDTO.java
+++ src/main/java/com/sanaiclub/user/model/dto/LoginRequestDTO.java
@@ -14,11 +14,9 @@ import javax.validation.constraints.Size;
 public class LoginRequestDTO {
 
     @NotBlank(message = "아이디를 입력해주세요.")
-    @Size(min = 4, max = 20, message = "아이디는 4~20자 사이여야 합니다.")
     private String loginId;
 
     @NotBlank(message = "비밀번호를 입력해주세요.")
-    @Size(min = 8, max = 100, message = "비밀번호는 8자 이상이어야 합니다.")
     private String password;
 
 }
